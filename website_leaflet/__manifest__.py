afa708b761b20fab637d57e0990978818443924f
Mathieu Benoit <mathieu.benoit@mathben.tech>
2020-05-01 02:42:03 -0400

    [ADD] website_leaflet: first commit

---
+++ website_leaflet/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright (C) 2020, SantéLibre
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Leaflet',
+    'summary': 'Leaflet Integration in website',
+    'version': '12.0.1.0.0',
+    'author': 'SantéLibre',
+    'license': 'AGPL-3',
+    'category': 'Extra Tools',
+    'depends': [
+        'website',
+    ],
+    'data': [
+    ],
+    'installable': True,
+    'application': True,
+}
