f10ddac3f177b212784fc68121bac36f05cac388
Gaël Kijko <gael@caravan.coop>
2020-05-02 19:48:36 -0400

    Add line and are in view

--- website_leaflet/models/models.py
+++ website_leaflet/models/models.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api, _
 
 
 class Category(models.Model):
@@ -16,12 +16,31 @@ class Category(models.Model):
     parent = fields.Many2one(comodel_name='website_leaflet.category', ondelete='restrict')
 
 
+class Map(models.Model):
+    _name = 'website_leaflet.map'
+    _description = 'Map'
+
+    active = fields.Boolean(string='Active', default=True)
+    name = fields.Char(string='Name', required=True)
+    description = fields.Char(string='Description')
+    company_id = fields.Many2one(
+        'res.company',
+        string="Company",
+    )
+    category = fields.Many2one(
+        comodel_name='website_leaflet.category', string="Company", ondelete='restrict')
+    features = fields.Many2many(
+        comodel_name='website_leaflet.mapfeature', string="Features")
+
+
 class MapFeature(models.Model):
     _name = 'website_leaflet.mapfeature'
     _description = 'Map Feature'
 
     active = fields.Boolean(string='Active', default=True)
     name = fields.Char(string='Name', required=True)
+    type = fields.Selection(
+        selection=[("point", _("Point")), ("line", _("Line")), ("poly", _("Polygon"))], required=True, default="point")
     geo_point = fields.GeoPoint()
     geo_line = fields.GeoLine()
     geo_area = fields.GeoPolygon()
