7e6eea9216bb382291c5a573be7c7d0c9c9a4336
Gaël Kijko <gael@caravan.coop>
2020-05-02 11:29:31 -0400

    Add MapFeature and Category models

--- website_leaflet/models/models.py
+++ website_leaflet/models/models.py
@@ -1,15 +1,27 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+import base_geoengine
 
-# class website_leaflet(models.Model):
-#     _name = 'website_leaflet.website_leaflet'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+class Category(models.Model):
+    _name = 'website_leaflet.category'
+
+    active = fields.Boolean(string='Active', default=True)
+    name = fields.Char(string='Name', required=True)
+    company_id = fields.Many2one(
+        'res.company',
+        string="Company",
+    )
+    parent = fields.Many2One(comodel_name='website_leaflet.category', ondelete='restrict')
+
+
+class MapFeature(models.Model):
+    _name = 'website_leaflet.mapfeature'
+
+    active = fields.Boolean(string='Active', default=True)
+    name = fields.Char(string='Name', required=True)
+    geo_point = base_geoengine.fields.GeoPoint()
+    geo_line = base_geoengine.fields.GeoLine()
+    geo_area = base_geoengine.fields.GeoPolygon()
+    category = fields.Many2One(comodel_name='website_leaflet.category', ondelete='restrict')
